723a9dde9ef5b4a45c7af5c113ceca0d87c6f2de
Katherine Bustamante <skatherine.bm@gmail.com>
2023-04-09 04:27:04 -0500

    se agregó home como ruta predeterminada

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ContextGlobalProvider from "./Components/utils/global.context";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, redirect } from "react-router-dom";
 import Contact from "./Routes/Contact";
 import Detail from "./Routes/Detail";
 import Favs from "./Routes/Favs";
@@ -15,11 +15,12 @@ root.render(
     <ContextGlobalProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />}>
+          <Route path="/" element={<App />} >
             <Route path="contact" element={<Contact />} />
             <Route path="detail/:id" element={<Detail />} />
             <Route path="favorites" element={<Favs />} />
             <Route path="home" element={<Home />} />
+            <Route path="" element={<Navigate to={"home"}/>} />
           </Route>
         </Routes>
       </BrowserRouter>
